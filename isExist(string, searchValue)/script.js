794e644fb454cc3a8f4b0cb4ddefd936efe0bfb6
Rimjhim Patidar <rimjhimpatidar26@gmail.com>
2025-07-28 19:57:21 +0530

    isExist(string, searchValue)

---
+++ isExist(string, searchValue)/script.js
@@ -0,0 +1,5 @@
+function isExist(str, searchValue) {
+    return str.includes(searchValue);
+  }
+  console.log(isExist("This is JavaScript code", "JavaScript"));
+  console.log(isExist("I am Rimjhim", "patidar"));
