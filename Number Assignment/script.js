e2117c9b4ab71e340db3324031eab980eaef33f5
Rimjhim Patidar <rimjhimpatidar26@gmail.com>
2025-07-30 19:25:31 +0530

    Basic Calculation

---
+++ Number Assignment/script.js
@@ -0,0 +1,23 @@
+// Addition Function
+function add(a,b){
+   return  a+b;
+}
+
+// Substraction Function
+function sub(a,b){
+    return a-b;
+}
+
+// Multiplication function
+function mult(a,b){
+    return a*b;
+}
+
+// Division Function
+function div(a,b){
+    return a/b;
+}
+console.log(add(5,6));
+console.log(sub(5,6));
+console.log(mult(5,6));
+console.log(div(5,6));
